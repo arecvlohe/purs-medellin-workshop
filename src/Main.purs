39404a87f10fdb4ea6079bc37c22ffcbf02aa44b
Adam Recvlohe <adam.recvlohe@gmail.com>
2017-11-01 17:43:26 -0500

    Part 3: Add Pux

--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,41 @@
 module Main where
 
-import Prelude
+import Prelude hiding (div)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Pux (CoreEffects, EffModel, start)
+import Pux.DOM.Events (onClick)
+import Pux.DOM.HTML (HTML)
+import Pux.Renderer.React (renderToDOM)
+import Text.Smolder.HTML (button, div, span)
+import Text.Smolder.Markup (text, (#!))
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+{- Define user actions -}
+data Event = Increment | Decrement
+
+{- Define state shape -}
+type State = Int
+
+{- Define state updates based on user actions -}
+foldp :: ∀ fx. Event -> State -> EffModel State Event fx
+foldp Increment n = { state: n + 1, effects: [] }
+foldp Decrement n = { state: n - 1, effects: [] }
+
+{- Define what the view renders -}
+view :: State -> HTML Event
+view count =
+  div do
+    button #! onClick (const Increment) $ text "Increment"
+    span $ text (show count)
+    button #! onClick (const Decrement) $ text "Decrement"
+
+{- Define the main function that runs the app -}
+main :: ∀ fx. Eff (CoreEffects fx) Unit
 main = do
-  log "Hello sailor!"
+  app <- start
+    { initialState: 0
+    , view
+    , foldp
+    , inputs: []
+    }
+
+  renderToDOM "#app" app.markup app.input
